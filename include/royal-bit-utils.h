acfeabb9f2a9d1492a07e46ee247e281c5428127
Joshua Weinstein <jweinstein@splunk.com>
2019-10-23 00:13:16 -0700

    bit tools

--- include/royal-bit-utils.h
+++ include/royal-bit-utils.h
@@ -12,5 +12,19 @@
 #define ROYAL_BIT_IS_SET_4(num) (((num) & (num-1) & (num-2) & (num-3) & (num-4)) == 0)
 #define ROYAL_BIT_IS_SET_5(num) (((num) & (num-1) & (num-2) & (num-3) & (num-4) \
                                   & (num-5)) == 0)
+                                  
+/**
+ * Counts the set bits in the \c set, stores result in \c total
+ */       
+#define ROYAL_BIT_SET_COUNT(set, total) while(set) { \
+    set &= (set-1); \
+    total++; \
+}
+     
+
+/**
+ * Checks that \c set1 is only contained in \c set2
+ */     
+#define ROYAL_BIT_INTER_ONLY(set1, set2) (((set1) & ~(set2)) == 0)
 
 #endif // ROYAL_BIT_UTILS_H
